9f17e295031d680e45fac7a175a5d9b8775bff0d
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-07-25 18:22:46 +0500

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+function starRating1() {
+    document.getElementById("ratingInput").style.display = 'block';
+    document.getElementById("dotText2").style.display = 'none';
+    document.getElementById("dotText3").style.display = 'none';
+    document.getElementById("dotText4").style.display = 'none';
+    document.getElementById("dotText5").style.display = 'none';
+    document.getElementById("dotText6").style.display = 'none';
+}
