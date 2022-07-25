4e35b91a0f1e2c48ef6b4369a7851aa827c34980
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-07-25 19:23:05 +0500

    update

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 function starRating1() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
+    star1.style.color = "Gold";
+    star2.style.color = "#000";
+    star3.style.color = "#000";
+    star4.style.color = "#000";
+    star5.style.color = "#000";
 }
 function starRating2() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
+    star1.style.color = "#000";
+    star2.style.color = "Gold";
+    star3.style.color = "#000";
+    star4.style.color = "#000";
+    star5.style.color = "#000";
 }
 function starRating3() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
+    star1.style.color = "#000";
+    star2.style.color = "#000";
+    star3.style.color = "Gold";
+    star4.style.color = "#000";
+    star5.style.color = "#000";
 }
 function starRating4() {
     document.getElementById("ratingInput").style.display = 'none';
     document.getElementById("myButton").style.display = 'block';
+    star1.style.color = "#000";
+    star2.style.color = "#000";
+    star3.style.color = "#000";
+    star4.style.color = "Gold";
+    star5.style.color = "#000";
 }
 function starRating5() {
     document.getElementById("ratingInput").style.display = 'none';
     document.getElementById("myButton").style.display = 'block';
+    star1.style.color = "#000";
+    star2.style.color = "#000";
+    star3.style.color = "#000";
+    star4.style.color = "#000";
+    star5.style.color = "Gold";
 }
