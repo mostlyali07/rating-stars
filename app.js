9165b171e7549f7983f57ae9dded5693efaab467
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-07-25 18:44:45 +0500

    update

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 function starRating1() {
     document.getElementById("ratingInput").style.display = 'block';
-    document.getElementById("dotText2").style.display = 'none';
-    document.getElementById("dotText3").style.display = 'none';
-    document.getElementById("dotText4").style.display = 'none';
-    document.getElementById("dotText5").style.display = 'none';
-    document.getElementById("dotText6").style.display = 'none';
+    document.getElementById("myButton").style.display = 'none';
+}
+function starRating2() {
+    document.getElementById("ratingInput").style.display = 'block';
+    document.getElementById("myButton").style.display = 'none';
+}
+function starRating3() {
+    document.getElementById("ratingInput").style.display = 'block';
+    document.getElementById("myButton").style.display = 'none';
+}
+function starRating4() {
+    document.getElementById("ratingInput").style.display = 'none';
+    document.getElementById("myButton").style.display = 'block';
+}
+function starRating5() {
+    document.getElementById("ratingInput").style.display = 'none';
+    document.getElementById("myButton").style.display = 'block';
 }
