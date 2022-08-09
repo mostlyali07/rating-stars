7f21c12aef948e7391e58187b78ac0d5420e8513
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-09 19:24:35 +0500

    update

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
 function starRating1() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
-    star1.style.color = "Gold";
+    star1.style.color = "#fff";
     star2.style.color = "#000";
     star3.style.color = "#000";
     star4.style.color = "#000";
     star5.style.color = "#000";
+    star1.style.background = "#FF3722";
+    star2.style.background = "#fff";
+    star3.style.background = "#fff";
+    star4.style.background = "#fff";
+    star5.style.background = "#fff";
+    star1.style.borderRadius = '8px';
 }
 function starRating2() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
-    star1.style.color = "#000";
-    star2.style.color = "Gold";
+    star1.style.color = "#fff";
+    star2.style.color = "#fff";
     star3.style.color = "#000";
     star4.style.color = "#000";
     star5.style.color = "#000";
+    star1.style.background = "#FF8622";
+    star2.style.background = "#FF8622";
+    star2.style.borderRadius = '50px';
 }
 function starRating3() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
-    star1.style.color = "#000";
-    star2.style.color = "#000";
-    star3.style.color = "Gold";
+    star1.style.color = "#FFCE00";
+    star2.style.color = "#FFCE00";
+    star3.style.color = "#FFCE00";
     star4.style.color = "#000";
     star5.style.color = "#000";
 }
 function starRating4() {
     document.getElementById("ratingInput").style.display = 'none';
     document.getElementById("myButton").style.display = 'block';
-    star1.style.color = "#000";
-    star2.style.color = "#000";
-    star3.style.color = "#000";
-    star4.style.color = "Gold";
+    star1.style.color = "#73CF11";
+    star2.style.color = "#73CF11";
+    star3.style.color = "#73CF11";
+    star4.style.color = "#73CF11";
     star5.style.color = "#000";
 }
 function starRating5() {
     document.getElementById("ratingInput").style.display = 'none';
     document.getElementById("myButton").style.display = 'block';
-    star1.style.color = "#000";
-    star2.style.color = "#000";
-    star3.style.color = "#000";
-    star4.style.color = "#000";
-    star5.style.color = "Gold";
+    star1.style.color = "#00B67A";
+    star2.style.color = "#00B67A";
+    star3.style.color = "#00B67A";
+    star4.style.color = "#00B67A";
+    star5.style.color = "#00B67A";
 }
