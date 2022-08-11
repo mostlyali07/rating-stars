07a414300221220db62452a9a1f3dc6d176ff1d5
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-11 17:05:30 +0500

    update

--- app.js
+++ app.js
@@ -1,22 +1,103 @@
 
-function starRating1() {
+function starRating5() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
+    document.getElementById("label-1").style.backgroundColor = 'unset';
+    document.getElementById("label-1").style.color = 'unset';
+    document.getElementById("label-1").style.borderRadius  = 'unset';
+    document.getElementById("label-2").style.backgroundColor = 'unset';
+    document.getElementById("label-2").style.color = 'unset';
+    document.getElementById("label-2").style.borderRadius  = 'unset';
+    document.getElementById("label-3").style.backgroundColor = 'unset';
+    document.getElementById("label-3").style.color = 'unset';
+    document.getElementById("label-3").style.borderRadius  = 'unset';
+    document.getElementById("label-4").style.backgroundColor = 'unset';
+    document.getElementById("label-4").style.color = 'unset';
+    document.getElementById("label-4").style.borderRadius  = 'unset';
+    document.getElementById("label-5").style.backgroundColor = '#FF3722';
+    document.getElementById("label-5").style.color = '#FFFFFF';
+    document.getElementById("label-5").style.borderRadius  = '8px';
+
+
+
 }
 
-function starRating2() {
+function starRating4() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
+    document.getElementById("label-1").style.backgroundColor = 'unset';
+    document.getElementById("label-1").style.color = 'unset';
+    document.getElementById("label-1").style.borderRadius  = 'unset';
+    document.getElementById("label-2").style.backgroundColor = 'unset';
+    document.getElementById("label-2").style.color = 'unset';
+    document.getElementById("label-2").style.borderRadius  = 'unset';
+    document.getElementById("label-3").style.backgroundColor = 'unset';
+    document.getElementById("label-3").style.color = 'unset';
+    document.getElementById("label-3").style.borderRadius  = 'unset';
+    document.getElementById("label-4").style.backgroundColor = '#FF8622';
+    document.getElementById("label-4").style.color = '#FFFFFF';
+    document.getElementById("label-4").style.borderRadius  = '8px';
+    document.getElementById("label-5").style.backgroundColor = '#FF8622';
+    document.getElementById("label-5").style.color = '#FFFFFF';
+    document.getElementById("label-5").style.borderRadius  = '8px';
+
+
+
 }
 function starRating3() {
     document.getElementById("ratingInput").style.display = 'block';
     document.getElementById("myButton").style.display = 'none';
+    document.getElementById("label-1").style.backgroundColor = 'unset';
+    document.getElementById("label-1").style.color = 'unset';
+    document.getElementById("label-1").style.borderRadius  = 'unset';
+    document.getElementById("label-2").style.backgroundColor = 'unset';
+    document.getElementById("label-2").style.color = 'unset';
+    document.getElementById("label-2").style.borderRadius  = 'unset';
+    document.getElementById("label-3").style.backgroundColor = '#FFCE00';
+    document.getElementById("label-3").style.color = '#FFFFFF';
+    document.getElementById("label-3").style.borderRadius  = '8px';
+    document.getElementById("label-4").style.backgroundColor = '#FFCE00';
+    document.getElementById("label-4").style.color = '#FFFFFF';
+    document.getElementById("label-4").style.borderRadius  = '8px';
+    document.getElementById("label-5").style.backgroundColor = '#FFCE00';
+    document.getElementById("label-5").style.color = '#FFFFFF';
+    document.getElementById("label-5").style.borderRadius  = '8px';
 }
-function starRating4() {
+function starRating2() {
     document.getElementById("ratingInput").style.display = 'none';
     document.getElementById("myButton").style.display = 'block';
+    document.getElementById("label-1").style.backgroundColor = 'unset';
+    document.getElementById("label-1").style.color = 'unset';
+    document.getElementById("label-1").style.borderRadius  = 'unset';
+    document.getElementById("label-2").style.backgroundColor = '#73CF11';
+    document.getElementById("label-2").style.color = '#FFFFFF';
+    document.getElementById("label-2").style.borderRadius  = '8px';
+    document.getElementById("label-3").style.backgroundColor = '#73CF11';
+    document.getElementById("label-3").style.color = '#FFFFFF';
+    document.getElementById("label-3").style.borderRadius  = '8px';
+    document.getElementById("label-4").style.backgroundColor = '#73CF11';
+    document.getElementById("label-4").style.color = '#FFFFFF';
+    document.getElementById("label-4").style.borderRadius  = '8px';
+    document.getElementById("label-5").style.backgroundColor = '#73CF11';
+    document.getElementById("label-5").style.color = '#FFFFFF';
+    document.getElementById("label-5").style.borderRadius  = '8px';
 }
-function starRating5() {
+function starRating1() {
     document.getElementById("ratingInput").style.display = 'none';
     document.getElementById("myButton").style.display = 'block';
+    document.getElementById("label-1").style.backgroundColor = '#00B67A';
+    document.getElementById("label-1").style.color = '#FFFFFF';
+    document.getElementById("label-1").style.borderRadius  = '8px';
+    document.getElementById("label-2").style.backgroundColor = '#00B67A';
+    document.getElementById("label-2").style.color = '#FFFFFF';
+    document.getElementById("label-2").style.borderRadius  = '8px';
+    document.getElementById("label-3").style.backgroundColor = '#00B67A';
+    document.getElementById("label-3").style.color = '#FFFFFF';
+    document.getElementById("label-3").style.borderRadius  = '8px';
+    document.getElementById("label-4").style.backgroundColor = '#00B67A';
+    document.getElementById("label-4").style.color = '#FFFFFF';
+    document.getElementById("label-4").style.borderRadius  = '8px';
+    document.getElementById("label-5").style.backgroundColor = '#00B67A';
+    document.getElementById("label-5").style.color = '#FFFFFF';
+    document.getElementById("label-5").style.borderRadius  = '8px';
 }
